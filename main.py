6a6f4e36067550b28cd89b9f820955b36e298a9b
Acilio SIlva <eng.acilio@gmail.com>
2018-09-17 01:24:03 +0100

    Validade the my_blockchain

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ my_blockchain = Blockchain()
 
 my_blockchain.add_block(new_transactions)
 my_blockchain.print_blocks()
-
 my_blockchain.chain[1].transactions = "fake_transactions"
+
+if(my_blockchain.validate_chain()):
+  print ('Everything ok')
+else:
+  print ('Not a valid Block of  Chain')
